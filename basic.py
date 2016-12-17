38dfb16eb1d560d7819eed64c32714f33304b430
PerlinWarp <perlinwarp@gmail.com>
2016-12-17 15:18:55 +0000

    Added numbers and expressions

--- basic.py
+++ basic.py
@@ -5,6 +5,7 @@ tokens = [] #List of tokens
 #Opens the file to be interpreted
 def open_file(filename):
 	data = open(filename, "r").read()
+	data += "<EOF>"
 	return data 
 
 #Lex checks for keywords, and tokens 
@@ -12,6 +13,9 @@ def lex(filecontents):
 	tok = ""
 	state = 0
 	string = ""
+	expr = ""
+	isexpr = False #is this is an expression or a number? 
+	n = "" #number
 	filecontents = list(filecontents)
 	for char in filecontents:
 		tok += char #Adding every letter to token to make it make sence
@@ -21,12 +25,30 @@ def lex(filecontents):
 				tok = ""
 			else:
 				tok = " " 
-		elif (tok == "\n"): #If we find a new line in the code
+		elif (tok == "\n" or tok == "<EOF>"): #Every new line or at the end of the file in the code
+			if expr != "" and isexpr == True :
+				tokens.append("EXPR:"+expr)
+				expr = ""
+			elif expr != "" and isexpr == 0:
+				#expr is a number
+				tokens.append("NUM:"+expr)
+				expr = ""
 			tok = ""
-		elif (tok == "PRINT"):
+		elif (tok == "PRINT" or tok == "print"):
 			print("Found a print")
 			tokens.append("PRINT")
-			tok = "" ##Reset te
+			tok = "" ##Reset token
+		elif  (tok == "0" or tok == "1" or tok == "2" or tok == "3" or tok == "4" or tok == "5" or tok == "6" or tok == "7" or tok == "8" or tok == "9"):
+			#If the token is a number
+			print("NUMBER")
+			expr += tok
+			#tokens.append("NUMBER:" + tok + "\"")
+			tok = ""
+		elif tok == "+":
+			isexpr = True
+			expr += "+"
+			tok = ""
+
 		elif tok == "\"": #Found a string
 			if state == 0:
 				state = 1
@@ -41,7 +63,7 @@ def lex(filecontents):
 			tok = ""
 
 	#DEBUGGING
-	#print(tokens)
+	print(tokens)
 	return tokens
 #Parser
 def parse(toks):
@@ -51,6 +73,7 @@ def parse(toks):
 		if(toks[i] + " " + toks[i+1][0:6] == "PRINT STRING"):
 			print(toks[i+1][7:]) #Prints the 6 character onwards
 			i+=2
+
 	
 
 
