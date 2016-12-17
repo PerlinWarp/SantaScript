ec7f1199bef3497a8b462fec8004306088dd1046
PerlinWarp <perlinwarp@gmail.com>
2016-12-17 14:28:28 +0000

    Added Parser

--- basic.py
+++ basic.py
@@ -1,5 +1,7 @@
 from sys import *
 
+tokens = [] #List of tokens
+
 #Opens the file to be interpreted
 def open_file(filename):
 	data = open(filename, "r").read()
@@ -14,25 +16,48 @@ def lex(filecontents):
 	for char in filecontents:
 		tok += char #Adding every letter to token to make it make sence
 		if (tok == " "):
+			if (state == 0):
+			##If there isnt a string null the space
+				tok = ""
+			else:
+				tok = " " 
+		elif (tok == "\n"): #If we find a new line in the code
 			tok = ""
 		elif (tok == "PRINT"):
 			print("Found a print")
+			tokens.append("PRINT")
 			tok = "" ##Reset te
 		elif tok == "\"": #Found a string
 			if state == 0:
 				state = 1
 			elif state == 1:
 				print("Found a string")
+				tokens.append("STRING:" + string + "\"")
 				string = ""
 				state = 0
+				tok = ""
 		elif state == 1: #We found a double quote 
-			string += char
+			string += tok
+			tok = ""
 
+	#DEBUGGING
+	#print(tokens)
+	return tokens
+#Parser
+def parse(toks):
+	print(toks)
+	i = 0 #Idk why he isnt using a for loop
+	while (i < len(toks)):
+		if(toks[i] + " " + toks[i+1][0:6] == "PRINT STRING"):
+			print(toks[i+1][7:]) #Prints the 6 character onwards
+			i+=2
+	
 
 
 def run():
 	data = open_file(argv[1])
-	lex(data)
+	toks = lex(data) #Gets the tokens from the lex
 	#Parser runs the tokens
+	parse(toks)
 
 run()
